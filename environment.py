b0f00d4bd258471e7d59b8eb5eef0aaeab2b874c
TollanBerhanu <tollanberhanu@gmail.com>
2023-08-23 17:10:54 +0300

    outsource evnironment variables

---
+++ environment.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ["TOGETHER_API_KEY"] = "ac17a88fb15afc19f632fc58d39d177814f3ead1d013f7adc9bce9f3ccf33580"
+os.environ["NGROK_AUTH_TOKEN"] = "2UKtqNC7pDrDKG272UqIOy4rvSm_2ezkSxzZ7LDUBey1S2dM6"
+
+os.environ["EMBEDDING_URL"] = 'https://hackingfaces.onrender.com/embed'
+os.environ["EMBEDDING_MODEL_SPACE_LINK"] = 'https://huggingface.co/spaces/tollan/instructor-xl'
+# os.environ["EMBEDDING_MODEL_SPACE_LINK"] = 'https://huggingface.co/spaces/tollan/sentence-transformers-embedding'
