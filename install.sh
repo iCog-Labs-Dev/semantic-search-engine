2fa3afcb766f37a6283b349fa38b51b170ec980a
TollanBerhanu <tollanberhanu@gmail.com>
2023-08-23 12:13:46 +0300

    add 'quiet' flags to all commands

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 # To download the slack exports from github
-pip install gitpython
+pip -q install gitpython
 # To use the vector db locally
 pip -q install langchain chromadb
 # To create an API
-pip install flask flask-cors
+pip -q install flask flask-cors
 # To get a hosted instance of LLaMA2 70B
 pip -q install --upgrade together
