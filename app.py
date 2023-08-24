e9ba33b06f123aae1bffec0276b867d1fd88b634
TollanBerhanu <tollanberhanu@gmail.com>
2023-08-24 16:14:40 +0300

    feat: class based app

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request
 from flask_cors import CORS
-from src.LLM.llama import semantic_search
-from src.database.chroma import *
+from src.llm.semantic_search import SemanticSearch as s
+# from src.database.chroma import *
 from environment import *
 
 port_no = 5000
 
 
 
-upsert_channels(['random', 'general'])
+# upsert_channels(['random', 'general'])
 
 app = Flask(__name__)
 
@@ -18,11 +18,11 @@ def semantic_search_query():
 
   if request.method == 'GET':
     query = request.args.get('query')
-    return semantic_search(query)
+    return s.semantic_search(query)
 
   elif request.method == 'POST':
     query = request.json['query']
-    return semantic_search(query)
+    return s.semantic_search(query)
   
 print(f"Server running on port {port_no}....")
 app.run(port=port_no)
