1b042e513baf3b403a66adbb98cfe04859bb3447
TollanBerhanu <tollanberhanu@gmail.com>
2023-08-28 16:12:10 +0300

    fix upsert

--- src/database/slack/slack.py
+++ src/database/slack/slack.py
@@ -1,18 +1,25 @@
 import json
 import math
-from src.database.crud import CRUD as chroma
-from src.database.slack.extract_data import ExtractData as extract
+from src.database.crud import CRUD
+from src.database.slack.extract_data import ExtractData
 
 class Slack:
 
     def __init__(self, collection, slack_data_path, embedding):
         self.collection = collection
-        self.slack_data_path = slack_data_path,
         self.embedding = embedding
+        self.slack_data_path = slack_data_path
+        self.crud = CRUD(
+            collection=collection
+        )
     
 
     # This will fetch the metadata from the slack archive (passing an empty array will fetch all channels' metadata)
     def upsert_channels(self, channel_names=[], step=15):
+
+        extract = ExtractData(
+            slack_data_path=self.slack_data_path
+        )
 
         channels = extract.get_all_channels()
 
@@ -22,7 +29,7 @@ class Slack:
         for idx, ch_name in enumerate(channel_names):
             print(f'Upserting channel { str(idx+1) } of { str(len(channel_names)) }: "{ch_name}" ... ')
 
-            channel_metadata = extract.extract_channel_metadata(self.slack_data_path, ch_name)
+            channel_metadata = extract.extract_channel_metadata(ch_name)
 
             if (channel_metadata.empty):
                 print('-> The channel is empty / doesn\'t exist!')
@@ -36,19 +43,20 @@ class Slack:
                 print(f'-> Embedding Batch { math.ceil(end/step) }/{ math.ceil(no_messages/step) } ...')
 
                 self.upsert_channel(channel_metadata[start:end])
+        
+        return 'Upsert Complete!'
 
 
     def upsert_channel(self, channel_metadata_batch):
     
-            channel_embeddings = self.embedding.embed_channel_messages(channel_metadata_batch['message'])
+        channel_embeddings = self.embedding.embed_channel_messages(channel_metadata_batch['message'])
 
-            parsed_channel_metadata = json.loads(channel_metadata_batch.to_json(orient="records")) # parse the channel metadata to json
+        parsed_channel_metadata = json.loads(channel_metadata_batch.to_json(orient="records")) # parse the channel metadata to json
 
-            # create IDs for each embedding to be stored on the vector database
-            # ids = [ (channel_name + str(ch)) for ch in enumerate(embeddings) ] ... [channelname_0 -> ... -> channelname_n]
-            ids = [ str(hash(metadata['message'])) for metadata in parsed_channel_metadata ]
-
-            chroma.upsert(self.collection, ids, channel_embeddings, channel_metadata_batch) # ###########################################################################################################################
+        # create IDs for each embedding to be stored on the vector database
+        ids = [ str(hash(metadata['message'])) for metadata in parsed_channel_metadata ]
+        
+        self.crud.upsert(ids=ids, embeddings=channel_embeddings, metadata=parsed_channel_metadata) # ###########################################################################################################################
 
 
         # upsert_channels() # upsert all channels
@@ -57,7 +65,7 @@ class Slack:
         print('Embedding query ...', end=' ')
         embedded_query = self.embedding.embed_query(query)
         print('Done!')
-        query_response = chroma.retrieve(self.collection, embedded_query, num_results, condition)
+        query_response = self.crud.retrieve(embedded_query, num_results, condition)
         # documents = query_response['documents']
         scores = query_response['distances'][0]
         metadatas = query_response['metadatas'][0]
