2961a1c3ea5a13785892352c623ed23df28d1346
TollanBerhanu <tollanberhanu@gmail.com>
2023-08-31 09:54:54 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 # To download the slack exports from github
-pip -q install gitpython
+pip install --upgrade gitpython
 # To use the vector db locally
-pip -q install langchain chromadb
+pip install --upgrade langchain chromadb
 # To create an API
-pip -q install flask flask-cors
+pip install --upgrade flask flask-cors
 # To get a hosted instance of LLaMA2 70B
-pip -q install --upgrade together
+pip install --upgrade together
+
+pip install --upgrade pandas
+
+pip install --upgrade pysqlite3-binary
