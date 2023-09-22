db157450adf1ad56030f2efe0acde89ad50a5161
TollanBerhanu <tollanberhanu@gmail.com>
2023-09-22 14:15:38 +0300

    chore: moved fetch_interval to constants

--- src/semantic_search_engine/constants.py
+++ src/semantic_search_engine/constants.py
@@ -10,6 +10,10 @@ CHROMA_HOST = os.environ.get("SS_CHROMA_DB_HOST") or "127.0.0.1"
 CHROMA_PORT = os.environ.get("SS_CHROMA_DB_PORT") or 5555
 CHROMA_COLLECTION = os.environ.get("SS_CHROMA_COLLECTION_NAME") or "messages"
 
+# Shelve
+MM_SHELVE_NAME= os.environ.get("MM_SHELVE_NAME") or "last_fetch_time"
+SLACK_SHELVE_NAME= os.environ.get("SLACK_SHELVE_NAME") or "slack_store"
+
 # LLM
 TOGETHER_API_KEY= os.environ.get("TOGETHER_API_KEY")
 TOGETHER_MODEL_NAME= os.environ.get("TOGETHER_MODEL_NAME") or "togethercomputer/llama-2-70b-chat"
@@ -19,4 +23,4 @@ MM_SERVER_URL= os.environ.get("MM_SERVER_URL") or "http://localhost:8065/api/v4"
 MM_USER_NAME= os.environ.get("MM_USER_NAME") or "Admin"
 MM_PASSWORD= os.environ.get("MM_PASSWORD") or "password"
 MM_PERSONAL_ACCESS_TOKEN= os.environ.get("MM_PERSONAL_ACCESS_TOKEN")
-MM_FETCH_INTERVAL= os.environ.get("MM_FETCH_INTERVAL")
+MM_FETCH_INTERVAL= os.environ.get("MM_FETCH_INTERVAL") or 5
