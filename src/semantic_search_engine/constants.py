8b931e8811c5bd474738248bd4a209072d1409ba
TollanBerhanu <tollanberhanu@gmail.com>
2023-09-25 14:58:36 +0300

    fix: add error handling

--- src/semantic_search_engine/constants.py
+++ src/semantic_search_engine/constants.py
@@ -23,12 +23,17 @@ TOGETHER_MODEL_NAME= os.environ.get("TOGETHER_MODEL_NAME") or "togethercomputer/
 # Mattermost
 MM_USER_NAME= os.environ.get("MM_USER_NAME") or "Admin"
 MM_PASSWORD= os.environ.get("MM_PASSWORD") or "password"
-# MM_SERVER_URL= os.environ.get("MM_SERVER_URL") or "http://localhost:8065/api/v4"
-# MM_FETCH_INTERVAL= os.environ.get("MM_FETCH_INTERVAL") or 5
-# MM_PERSONAL_ACCESS_TOKEN= os.environ.get("MM_PERSONAL_ACCESS_TOKEN")
+
 
 # Constants from shelve
-with shelve.open(SETTINGS_SHELVE_NAME) as settings:
-    MM_SERVER_URL= (settings['mattermost-url'] + '/api/v4') or 'http://localhost:8065/api/v4'
-    MM_FETCH_INTERVAL= (settings['fetch-interval'] * 60) or 5       # fetch interval in seconds 
-    MM_PERSONAL_ACCESS_TOKEN= settings['personal-access-token']
+try:
+    with shelve.open(SETTINGS_SHELVE_NAME) as settings:
+        MM_SERVER_URL= (settings['mattermost-url'] + '/api/v4') or 'http://localhost:8065/api/v4'
+        MM_FETCH_INTERVAL= (settings['fetch-interval'] * 60) or 5       # fetch interval in seconds 
+        MM_PERSONAL_ACCESS_TOKEN= settings['personal-access-token'] or ''
+except:
+    print(f'The shelve "{SETTINGS_SHELVE_NAME}" doesn\'t exist and will be created!')
+    MM_SERVER_URL= os.environ.get("MM_SERVER_URL") or "http://localhost:8065/api/v4"
+    MM_FETCH_INTERVAL= os.environ.get("MM_FETCH_INTERVAL") or 5
+    MM_PERSONAL_ACCESS_TOKEN= os.environ.get("MM_PERSONAL_ACCESS_TOKEN")
+
