7fc257f5a4ddd61f2112f637f6f39d0151830b9d
TollanBerhanu <tollanberhanu@gmail.com>
2023-10-04 16:52:19 +0300

    fix: changed constant names

--- src/semantic_search_engine/constants.py
+++ src/semantic_search_engine/constants.py
@@ -5,7 +5,7 @@ import shelve
 load_dotenv()
 
 # Chroma
-DIR = os.path.dirname(os.path.abspath(__file__))
+DIR =  os.getcwd() #os.path.dirname(os.path.abspath(__file__))
 CHROMA_PATH = os.environ.get("SS_CHROMA_DB_PATH") or os.path.join(DIR, 'chroma_db')
 CHROMA_HOST = os.environ.get("SS_CHROMA_DB_HOST") or "127.0.0.1"
 CHROMA_PORT = os.environ.get("SS_CHROMA_DB_PORT") or 5555
@@ -23,22 +23,25 @@ TOGETHER_MODEL_NAME= os.environ.get("TOGETHER_MODEL_NAME") or "togethercomputer/
 MM_USER_NAME= os.environ.get("MM_USER_NAME") or "Admin"
 MM_PASSWORD= os.environ.get("MM_PASSWORD") or "password"
 
+# Sqlite
+SQLITE_PATH = os.environ.get("SQLITE_PATH") or os.path.join(DIR, 'sqlite')
+TEMP_SLACK_DATA_PATH = os.path.join(SQLITE_PATH, 'temp/')
 
 # Constants from shelve
 with shelve.open(SETTINGS_SHELVE_NAME) as settings:
-    if 'mattermost-api-url' in settings:
-        MM_API_URL= (settings['mattermost-api-url']) or "http://localhost:8065/api/v4"
+    if 'mattermost_api_url' in settings:
+        MM_API_URL= (settings['mattermost_api_url']) or "http://localhost:8065/api/v4"
     else:
         MM_API_URL= os.environ.get("MM_API_URL") or "http://localhost:8065/api/v4"
 
-    # if 'fetch-interval' in settings:
-    #     MM_FETCH_INTERVAL= int(settings['fetch-interval']) or 5       # fetch interval in seconds
-    # else:
-    #     MM_FETCH_INTERVAL= os.environ.get("MM_FETCH_INTERVAL") or 5
+    if 'mattermost_url' in settings:
+        MM_URL= (settings['mattermost_url']) or "http://localhost:8065/"
+    else:
+        MM_URL= os.environ.get("MM_URL") or "http://localhost:8065/"
 
     # TODO: This should be removed
-    if 'personal-access-token' in settings:
-        MM_PERSONAL_ACCESS_TOKEN= settings['personal-access-token']
+    if 'personal_access_token' in settings:
+        MM_PERSONAL_ACCESS_TOKEN= settings['personal_access_token']
     else:
         MM_PERSONAL_ACCESS_TOKEN= os.environ.get("MM_PERSONAL_ACCESS_TOKEN") or ''
 
