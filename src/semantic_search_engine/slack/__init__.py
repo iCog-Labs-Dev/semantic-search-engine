1672794f1df80bfe4978b8bca119e8b4e70469d5
Tollan Sitotaw <tollanberhanu@gmail.com>
2023-10-07 19:01:04 +0300

    chore: move db initialization to __init__.py

--- src/semantic_search_engine/slack/__init__.py
+++ src/semantic_search_engine/slack/__init__.py
@@ -1,4 +1,11 @@
 import os
-from semantic_search_engine.constants import TEMP_SLACK_DATA_PATH
+from peewee import SqliteDatabase
+from semantic_search_engine.constants import TEMP_SLACK_DATA_PATH, SQLITE_PATH
 
 os.makedirs(TEMP_SLACK_DATA_PATH, exist_ok=True)
+
+# get or create connection with SQLite database
+db = SqliteDatabase(
+   os.path.join(SQLITE_PATH, '/slack.db'), pragmas={'journal_mode': 'wal', 'cache_size': 10000,'foreign_keys': 1}
+)
+print('Path to local db:', db.database)
