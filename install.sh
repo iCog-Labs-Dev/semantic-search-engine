64ef09e6ada111b04cfa121d211e916b04b72b4e
TollanBerhanu <tollanberhanu@gmail.com>
2023-11-02 14:57:08 +0300

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install --upgrade pip
+pip install requests==2.31.0
+pip install flask==3.0.0 flask-cors==4.0.0
+pip install chromadb==0.4.12
+pip install together==0.1.5
+pip install langchain==0.0.277
+pip install peewee==3.16.3
+pip install pydantic==1.10.12
