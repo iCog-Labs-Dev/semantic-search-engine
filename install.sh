6d52242d2e08d4849bc3836e55507833fa82cbac
TollanBerhanu <tollanberhanu@gmail.com>
2023-11-02 15:12:01 +0300

    remove venv

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 pip install --upgrade pip
 pip install requests==2.31.0
 pip install flask==3.0.0 flask-cors==4.0.0
-pip install chromadb==0.4.12
+pip install chromadb
 pip install together==0.1.5
 pip install langchain==0.0.277
 pip install peewee==3.16.3
