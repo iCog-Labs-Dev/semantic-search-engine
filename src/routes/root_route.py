d9a2b3be2bbce93d924e389ce04e6a172226b4bc
TollanBerhanu <tollanberhanu@gmail.com>
2023-11-02 10:57:58 +0300

    refactor: use shelves module

--- src/routes/root_route.py
+++ src/routes/root_route.py
@@ -1,9 +1,10 @@
 import shelve
 from flask import Response, Blueprint
 from json import dumps as to_json
-from semantic_search_engine.constants import FETCH_INTERVAL_SHELVE, LAST_FETCH_TIME_SHELVE, CHROMA_SHELVE
+from semantic_search_engine.constants import SYNC_INTERVAL_SHELVE, LAST_SYNC_TIME_SHELVE, CHROMA_SHELVE
 from routes.login_decorator import login_required
 from semantic_search_engine.mattermost.fetch_mm_data import is_sync_inprogress
+from semantic_search_engine.shelves import retrieve_one
 from . import mattermost
 
 root_bp = Blueprint("root", __name__)
@@ -20,22 +21,31 @@ def ping():
 @login_required(admin_only=True)
 def root_route(loggedin_user):
     try:
-        res = {}
-        with shelve.open(FETCH_INTERVAL_SHELVE) as fetch_interval_db:
-            res['fetch_interval'] = fetch_interval_db[FETCH_INTERVAL_SHELVE]
+        # res = {}
+        # with shelve.open(SYNC_INTERVAL_SHELVE) as sync_interval_db:
+        #     res['sync_interval'] = sync_interval_db[SYNC_INTERVAL_SHELVE]
 
-        with shelve.open(LAST_FETCH_TIME_SHELVE) as last_fetch_time_db:
-            res['last_fetch_time'] = last_fetch_time_db[LAST_FETCH_TIME_SHELVE] * 1000
+        # with shelve.open(LAST_SYNC_TIME_SHELVE) as last_sync_time_db:
+        #     res['last_sync_time'] = last_sync_time_db[LAST_SYNC_TIME_SHELVE] * 1000
         
-        with shelve.open( CHROMA_SHELVE ) as chroma_n_results_db:
-            res['chroma_n_results'] =  chroma_n_results_db['chroma_n_results']
-            res['max_chroma_distance'] =  chroma_n_results_db['max_chroma_distance']
+        # with shelve.open( CHROMA_SHELVE ) as chroma_n_results_db:
+        #     res['chroma_n_results'] =  chroma_n_results_db['chroma_n_results']
+        #     res['max_chroma_distance'] =  chroma_n_results_db['max_chroma_distance']
             
-        res['is_syncing'] = mattermost.is_syncing()
-        res['in_progress'] = is_sync_inprogress()
+        # res['is_syncing'] = mattermost.is_syncing()
+        # res['in_progress'] = is_sync_inprogress()
+
+        res = {
+            'sync_interval' : retrieve_one( shelve_name=SYNC_INTERVAL_SHELVE, key='sync_interval' ),
+            'last_sync_time' : float(retrieve_one( shelve_name=LAST_SYNC_TIME_SHELVE, key='last_sync_time' )) * 1000,
+            'chroma_n_results' :  retrieve_one( shelve_name=CHROMA_SHELVE , key='chroma_n_results' ),
+            'max_chroma_distance' :  retrieve_one( shelve_name=CHROMA_SHELVE, key='max_chroma_distance' ),
+            'is_syncing' : mattermost.is_syncing(),
+            'in_progress' : is_sync_inprogress()
+        }
 
         return Response(to_json(res), status=200, mimetype='application/json')
-    
+
     except Exception as err:
         return Response(to_json({
             'message': 'Something went wrong! Please restart the server.',
