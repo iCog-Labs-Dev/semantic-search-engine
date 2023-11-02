a597ac2cd9441cb8a8cf0060a36d9b848355059c
TollanBerhanu <tollanberhanu@gmail.com>
2023-11-02 15:54:01 +0300

    remove versions

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 pip install --upgrade pip
-pip install requests==2.31.0
-pip install flask==3.0.0 flask-cors==4.0.0
+pip install requests
+pip install flask flask-cors
 pip install chromadb
-pip install together==0.1.5
-pip install langchain==0.0.277
-pip install peewee==3.16.3
-pip install pydantic==1.10.12
+pip install together
+pip install langchain
+pip install peewee
