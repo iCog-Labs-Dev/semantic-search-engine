117c279a3e8ad76b2006a11689d20e0a4dc9eb02
TollanBerhanu <tollanberhanu@gmail.com>
2023-11-02 10:57:02 +0300

    refactor: add create PAT func

--- src/semantic_search_engine/mattermost/mm_api.py
+++ src/semantic_search_engine/mattermost/mm_api.py
@@ -1,6 +1,7 @@
-import os, requests, json
+import os, requests, json, shelve
 from semantic_search_engine.constants import MM_PAT_ID_SHELVE
 from dotenv import load_dotenv
+from semantic_search_engine.shelves import retrieve_one, store
 
 load_dotenv()
 
@@ -33,3 +34,40 @@ class MattermostAPI:
             raise Exception(f"Request to '{route}' failed with status code: ", res.status_code)
             
         return res.json()
+
+    def create_new_pat(self) -> str:
+        pat_id = retrieve_one(
+            shelve_name=MM_PAT_ID_SHELVE,
+            key='personal_access_token_id'
+        )
+        # with shelve.open( MM_PAT_ID_SHELVE ) as pat_id:
+
+        # if pat_id.get( MM_PAT_ID_SHELVE, False ):
+        if pat_id:
+            # Delete the prev pat
+            res = self.mm_api_request(
+                route='/users/tokens/revoke',
+                method='POST',
+                data={ 'token_id': pat_id }
+            )
+            if not res.get('status') == 'OK':
+                print("Failed to delete previous personal access token!")
+
+        # Create a new personal_access_token
+        res = self.mm_api_request(
+            route='/users/me/tokens',
+            method='POST',
+            data={ 'description': 'Mattermost Semantic Search' }
+        )
+        print(res)
+        if not (res.get('id') or res.get('token')):
+            raise Exception('Failed to create a new personal access token!')
+        
+        # Update the personal_access_token's id in shelve
+        store(
+            shelve_name=MM_PAT_ID_SHELVE,
+            personal_access_token_id=res['id']
+        )
+        # pat_id[ MM_PAT_ID_SHELVE ] = res['id']
+
+        return res['token']
